363bf800cad67848e383386b544a83ea7c3e80fe
ghousemohamed <ghousemohamed7126@gmail.com>
2020-04-15 19:29:42 +0530

    deploy

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -11,6 +11,22 @@ database_url =
     For example: ecto://USER:PASS@HOST/DATABASE
     """
 
+config :demo_heroku_deployment, DemoWeb.Endpoint,
+    load_from_system_env: true,
+    # Don't forget to replace pure-peak-67829 with the name of your Phoenix application that was created.
+    url: [scheme: "https", host: "fathomless-stream-80884.herokuapp.com", port: 80],
+    cache_static_manifest: "priv/static/cache_manifest.json",
+    secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  
+  config :demo_heroku_deployment, DemoWeb.Repo,
+    adapter: Ecto.Adapters.Postgres,
+      url: System.get_env("DATABASE_URL"),
+      pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+      ssl: true
+  
+  
+  config :logger, level: :info
+
 config :demo, Demo.Repo,
   ssl: true,
   url: database_url,
