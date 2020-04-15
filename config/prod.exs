5c3583c295f9e179d01488f2f9634ecbcfc39643
ghousemohamed <ghousemohamed7126@gmail.com>
2020-04-15 19:33:41 +0530

    deploy

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,7 @@ config :demo, DemoWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "fathomless-stream-80884.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
