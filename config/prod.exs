c96c12f8b5b6af66fb4dfc1c86772116d75ee2b5
Ronak Gothi <ronakjain90@gmail.com>
2020-04-27 14:40:06 +0530

    Changed prod config

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,11 @@ use Mix.Config
 # before starting your production server.
 config :demo, DemoWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "fathomless-stream-80884.herokuapp.com", port: 443],
+  url: [
+    scheme: "https",
+    host: "phoenix-demo.acuments.com",
+    port: 443
+  ],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
