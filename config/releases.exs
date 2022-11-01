7aefbabfa637b3079202bbfe0eddeea002f09919
Ronak Gothi <ronak@acuments.com>
2022-11-01 10:48:04 +0530

    Preparing deploy

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
   config :logger, level: :info
 
@@ -25,7 +25,7 @@ config :demo, DemoWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :demo, DemoWeb.Endpoint, server: true
+     config :demo, DemoWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
