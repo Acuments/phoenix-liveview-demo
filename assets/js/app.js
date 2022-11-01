266d1849559e0ca25332775b32d9efe2a6d65cd0
Ronak Gothi <ronak@acuments.com>
2022-11-01 11:49:40 +0530

    fixixng build

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
-// assets/js/app.js
+// We need to import the CSS so that webpack will load it.
+// The MiniCssExtractPlugin is used to separate it out into
+// its own CSS file.
+import "../css/app.css"
+import "phoenix_html";
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 
